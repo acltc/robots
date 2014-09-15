fe0e7fce49ff16c6dd2f3b1fad59569c69d4b93e
Jay Wengrow <jaywngrw@gmail.com>
2014-09-15 17:53:26 -0500

    sphero keyboard and multiple

---
+++ sphero/keyboard.js
@@ -0,0 +1,44 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: [
+    { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-BBP-AMP-SPP' },
+    { name: 'keyboard', adaptor: 'keyboard' }
+    ],
+  devices: [
+    {name: 'sphero', driver: 'sphero', connection: 'sphero'},
+    {name: 'keyboard', driver: 'keyboard', connection: 'keyboard'}
+  ],
+
+  work: function(my) {
+    my.keyboard.on('right', function(key) { 
+      console.log("RIGHT PRESSED!");
+      my.sphero.setRandomColor();
+      my.sphero.roll(150, 90);
+    });
+
+    my.keyboard.on('up', function(key) { 
+      console.log("UP PRESSED!");
+      my.sphero.setRandomColor();
+      my.sphero.roll(150, 0);
+    });
+
+    my.keyboard.on('down', function(key) { 
+      console.log("DOWN PRESSED!");
+      my.sphero.setRandomColor();
+      my.sphero.roll(150, 180);
+    });
+
+    my.keyboard.on('left', function(key) { 
+      console.log("LEFT PRESSED!");
+      my.sphero.setRandomColor();
+      my.sphero.roll(150, 270);
+    });
+
+    my.keyboard.on('space', function(key) { 
+      console.log("SPACE PRESSED!");
+      my.sphero.setRandomColor();
+      my.sphero.stop();
+    });
+  }
+}).start();
---
+++ sphero/multiple.js
@@ -0,0 +1,23 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-BBP-AMP-SPP' },
+  device: { name: 'sphero', driver: 'sphero' },
+
+  work: function(my) {
+    every((1).second(), function() {
+      my.sphero.roll(60, Math.floor(Math.random() * 360));
+    });
+  }
+}).start();
+
+Cylon.robot({
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-WWB-AMP-SPP' },
+  device: { name: 'sphero', driver: 'sphero' },
+
+  work: function(my) {
+    every((1).second(), function() {
+      my.sphero.roll(60, Math.floor(Math.random() * 360));
+    });
+  }
+}).start();
