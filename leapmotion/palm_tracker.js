a8d8c2581533511778806922fed28055676d5c95
Jay Wengrow <jaywngrw@gmail.com>
2014-11-08 20:31:20 -0600

    more work on leapmotion

---
+++ leapmotion/palm_tracker.js
@@ -0,0 +1,20 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { name: 'leapmotion', adaptor: 'leapmotion' },
+  device: { name: 'leapmotion', driver: 'leapmotion' },
+
+  work: function(my) {
+    my.leapmotion.on('hand', function(hand) {
+      var palmPosition = hand.palmPosition;
+      console.log("PALM POSITION:");
+      console.log("X:")
+      console.log(palmPosition[0]);
+      console.log("Y:")
+      console.log(palmPosition[2]);
+      console.log("Z:")
+      console.log(palmPosition[1]);
+    });
+
+  }
+}).start();
---
+++ leapmotion/test.js
@@ -0,0 +1,31 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { name: 'leapmotion', adaptor: 'leapmotion' },
+  device: { name: 'leapmotion', driver: 'leapmotion' },
+
+  work: function(my) {
+    // my.leapmotion.on('frame', function(frame) {
+    //   console.log("FRAME");
+    //   console.log(frame.toString());
+    // });
+
+    my.leapmotion.on('hand', function(hand) {
+      var palmPosition = hand.palmPosition;
+      console.log("PALM POSITION:");
+      console.log("X:")
+      console.log(palmPosition[0]);
+      console.log("Y:")
+      console.log(palmPosition[2]);
+      console.log("Z:")
+      console.log(palmPosition[1]);
+      // console.log(hand.toString());
+    });
+
+    // my.leapmotion.on('gesture', function(gesture) {
+    //   console.log("GESTURE");
+    //   console.log(gesture.type);
+    //   console.log(gesture.toString());
+    // });
+  }
+}).start();
